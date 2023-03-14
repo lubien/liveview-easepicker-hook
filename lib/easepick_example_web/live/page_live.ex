851e656957f7db375c11aaa8cb4b79b10b6e0325
lubien <lubien1996@gmail.com>
2023-03-14 13:49:41 -0300

    Add PageLive

---
+++ lib/easepick_example_web/live/page_live.ex
@@ -0,0 +1,10 @@
+defmodule EasepickExampleWeb.PageLive do
+  use EasepickExampleWeb, :live_view
+
+  def render(assigns) do
+    ~H"""
+    <div>
+    </div>
+    """
+  end
+end
--- lib/easepick_example_web/router.ex
+++ lib/easepick_example_web/router.ex
@@ -17,7 +17,7 @@ defmodule EasepickExampleWeb.Router do
   scope "/", EasepickExampleWeb do
     pipe_through :browser
 
-    get "/", PageController, :home
+    live "/", PageLive, :home
   end
 
   # Other scopes may use custom stacks.
